65a5d28a2ee27e0cf4ed20db5ec631b43295cdeb
Preedee Ponchevin <nickmsft@mac.com>
2023-08-21 20:47:11 +0700

    Add - Flow for ID Token - Display information in page Profile

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use oauth2::{
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::env;
-use std::fs::File;
 use std::io::Read;
 use std::str::FromStr;
 use std::sync::Arc;
